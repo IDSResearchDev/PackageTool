ba87bf19df7d3bf858621ca0b7667de03dc0825c
matt <mattronilomendez@live.com.ph>
2016-10-07 09:47:26 +0800

    Add Validation on Scale Value

--- PackageTool/View/MainView.xaml.cs
+++ PackageTool/View/MainView.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using PackageTool.ViewModel;
 using Rnd.Common;
+using System.Text.RegularExpressions;
 
 namespace PackageTool.View
 {
@@ -21,7 +22,28 @@ namespace PackageTool.View
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
-            this.VM.IsKeyLeftControl = e.Key == Key.LeftCtrl;            
+            this.VM.IsKeyLeftControl = e.Key == Key.LeftCtrl;
+                     
+        }
+        
+        private void ValidateNumeric_OnTxtChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            Int32 selectionStart = textBox.SelectionStart;
+            Int32 selectionLength = textBox.SelectionLength;
+            String newText = String.Empty;
+            int count = 0;
+            foreach (Char c in textBox.Text.ToCharArray())
+            {
+                if (Char.IsDigit(c) || Char.IsControl(c) || (c == '.' && count == 0))
+                {
+                    newText += c;
+                    if (c == '.')
+                        count += 1;
+                }
+            }
+            textBox.Text = newText;
+            textBox.SelectionStart = selectionStart <= textBox.Text.Length ? selectionStart : textBox.Text.Length;
         }
     }
 }
