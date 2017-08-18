eb2faa7736483d19335e97c0a61f2ad8e3a0abb9
froy lim <lim.froy@gmail.com>
2017-08-18 12:30:05 +0800

    no message

--- PackageTool/Properties/AssemblyInfo.cs
+++ PackageTool/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyTitle("PackageTool")]
 [assembly: AssemblyDescription("TeklaStructure drawing export tool")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("IDS, Inc")]
+[assembly: AssemblyCompany("IDS Inc")]
 [assembly: AssemblyProduct("PackageTool")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
