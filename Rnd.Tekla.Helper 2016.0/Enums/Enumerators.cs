82808cafcd60891a94ecb9e26769d1d8d324034f
froy lim <lim.froy@gmail.com>
2017-09-11 16:10:57 +0800

    Tekla 2016

---
+++ Rnd.Tekla.Helper 2016.0/Enums/Enumerators.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rnd.TeklaStructure.Helper.Enums
+{
+    public enum ExportType
+    {
+        PDF,
+        DXF,
+        DWG
+    }
+
+    public enum DrawingType
+    {
+        A,
+        C,
+        G,
+        M,
+        W
+    }
+
+    public enum BoltType
+    {
+        Site /*= 1*/,
+        WorkShop /*= 2*/,
+    }
+}
