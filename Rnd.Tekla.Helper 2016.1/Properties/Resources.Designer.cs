ae6dbb01a30a13a46fae16bc96ff46adf3b6c4f8
froy lim <lim.froy@gmail.com>
2017-09-12 10:02:55 +0800

    - Tekla 2016i (2016.1)

--- Rnd.Tekla.Helper 2016.1/Properties/Resources.Designer.cs
+++ Rnd.Tekla.Helper 2016.1/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PackageTool.Properties {
+namespace Rnd.TeklaStructure.Helper.Properties {
     using System;
     
     
@@ -22,7 +22,7 @@ namespace PackageTool.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Resources {
+    internal class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,10 +36,10 @@ namespace PackageTool.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Resources.ResourceManager ResourceManager {
+        internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PackageTool.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Rnd.TeklaStructure.Helper.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -51,7 +51,7 @@ namespace PackageTool.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Globalization.CultureInfo Culture {
+        internal static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -61,57 +61,12 @@ namespace PackageTool.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to http://webservice2.intdesignservices.com/.
+        ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
-        public static string ActivationUrl {
+        internal static byte[] KISS1_1_Export_mdd {
             get {
-                return ResourceManager.GetString("ActivationUrl", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot; ?&gt; 
-        ///&lt;recentdata&gt;
-        ///  &lt;!-- Export tab --&gt;
-        ///  &lt;version&gt;1.1&lt;/version&gt;
-        ///  &lt;type /&gt;
-        ///  &lt;cfg /&gt;
-        ///  &lt;packagedirectory /&gt;
-        ///  &lt;date /&gt;
-        ///  &lt;printerinstance /&gt;
-        ///  &lt;!-- Transmittal tab --&gt;
-        ///  &lt;projectnumber /&gt;
-        ///  &lt;transmittalnumber /&gt;
-        ///  &lt;projectname /&gt;
-        ///  &lt;location /&gt;
-        ///  &lt;remarks /&gt;
-        ///  &lt;attention /&gt;
-        ///  &lt;signatory /&gt;
-        ///  &lt;outputdirectory /&gt;
-        ///&lt;/recentdata&gt;
-        ///.
-        /// </summary>
-        public static string pkgrecentdata {
-            get {
-                return ResourceManager.GetString("pkgrecentdata", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to http://idsftpserver.com/rd/update/.
-        /// </summary>
-        public static string ServerPath {
-            get {
-                return ResourceManager.GetString("ServerPath", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to 2016.1.
-        /// </summary>
-        public static string TeklaTargetVersion {
-            get {
-                return ResourceManager.GetString("TeklaTargetVersion", resourceCulture);
+                object obj = ResourceManager.GetObject("KISS1_1_Export_mdd", resourceCulture);
+                return ((byte[])(obj));
             }
         }
     }
---
+++ Rnd.Tekla.Helper 2016.1/UnitConverter.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Rnd.TeklaStructure.Helper
+{
+    public static class UnitConverter
+    {
+        public static string ConvertDecimaltoFraction(this double value)
+        {
+            var val = (Convert.ToDouble(value) / 25.4) * 16.0;
+            var roundedvalue = (Math.Round(val, MidpointRounding.AwayFromZero) / 16.0);
+            decimal d = Convert.ToDecimal(roundedvalue).HandleNegative();
+            decimal num2 = Math.Truncate(d);
+            decimal num3 = d - num2;
+            uint valB = 0;
+            uint result = 1;
+            if (num3 > 0M)
+            {
+                string s = num3.ToString(System.Globalization.CultureInfo.InvariantCulture).Remove(0, 2);
+                uint length = (uint)s.Length;
+                valB = (uint)Math.Pow(10.0, (double)length);
+                uint.TryParse(s, out result);
+                uint num7 = GreatestCommonDivisor(result, valB);
+                valB /= num7;
+                result /= num7;
+            }
+            StringBuilder builder = new StringBuilder();
+            if (num2 > 0M)
+            {
+                builder.Append(num2);
+
+            }
+            if (num3 > 0M)
+            {
+                if (result / valB != 1)
+                {
+                    builder.Append(" ");
+                    builder.Append(result);
+                    builder.Append("/");
+                    builder.Append(valB);
+                }
+            }
+            builder.Append("\"");
+            return builder.ToString();
+        }
+        private static decimal HandleNegative(this decimal value)
+        {
+            if (value.ToString().Contains("-"))
+            {
+                return Convert.ToDecimal(value.ToString().Replace("-", ""));
+            }
+            return value;
+        }
+        private static uint GreatestCommonDivisor( uint valA, uint valB)
+        {
+            if ((valA == 0) && (valB == 0))
+            {
+                return 0;
+            }
+            if ((valA == 0) && (valB != 0))
+            {
+                return valB;
+            }
+            if ((valA != 0) && (valB == 0))
+            {
+                return valA;
+            }
+            uint num = valA;
+            uint num2 = valB;
+            while (num != num2)
+            {
+                if (num > num2)
+                {
+                    num -= num2;
+                }
+                else
+                {
+                    num2 -= num;
+                }
+            }
+            return num;
+        }
+
+        public static double ToDouble(this string value)
+        {
+            return Convert.ToDouble(value);
+        }
+
+        public static int Round0To5(this double value)
+        {
+
+            var toIntValue = 0;
+            var toStrValue = value.ToString(CultureInfo.InvariantCulture);
+            var split = toStrValue.Split('.');
+
+
+            toIntValue = Convert.ToInt32(split[0]);
+
+
+            var mod = toIntValue % 10;
+            var num = 0;
+
+
+            if (mod >= 1 && mod <= 4)
+            {
+                num = 5 - mod;
+                return Convert.ToInt32(toIntValue + num);
+            }
+            if (mod >= 6 && mod <= 9)
+            {
+                num = 10 - mod;
+                return Convert.ToInt32(toIntValue + num);
+            }
+
+
+            return Convert.ToInt32(toIntValue);
+        }
+    }
+}
